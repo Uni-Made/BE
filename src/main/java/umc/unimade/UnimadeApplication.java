e5cb86ea21c218c0a7a6dfc6264fa06433c5f6b5
gomin0 <gomin0981113@naver.com>
2024-07-12 23:14:48 +0900

    first commit

---
+++ src/main/java/umc/unimade/UnimadeApplication.java
@@ -0,0 +1,13 @@
+package umc.unimade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimadeApplication.class, args);
+	}
+
+}
