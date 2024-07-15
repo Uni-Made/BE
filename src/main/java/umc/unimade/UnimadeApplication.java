1de26396363e7d601b7d600ef0604e9cadf1c95a
gomin0 <gomin0981113@naver.com>
2024-07-16 01:10:48 +0900

    :sparkles: feat: RestTemplate 등록 (#3)

--- src/main/java/umc/unimade/UnimadeApplication.java
+++ src/main/java/umc/unimade/UnimadeApplication.java
@@ -2,14 +2,23 @@ package umc.unimade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
+@EntityScan
 @EnableJpaAuditing
 @SpringBootApplication
 public class UnimadeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnimadeApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
