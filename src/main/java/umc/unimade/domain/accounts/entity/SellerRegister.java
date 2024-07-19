96e6ffdcf5215a5ed0942bf7f8d6469a6a322d61
moonyaeyoon <moonyaeyoon@naver.com>
2024-07-20 02:10:06 +0900

    :lipstick: style : register파일 주석해제

--- src/main/java/umc/unimade/domain/accounts/entity/SellerRegister.java
+++ src/main/java/umc/unimade/domain/accounts/entity/SellerRegister.java
@@ -1,51 +1,51 @@
-//package umc.unimade.domain.accounts.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//import umc.unimade.global.registerStatus.RegisterStatus;
-//
-//@Entity
-//@Builder
-//@Getter
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
-//@AllArgsConstructor(access = AccessLevel.PRIVATE)
-//@Table(name = "seller_register")
-//public class SellerRegister {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "request_id", nullable = false)
-//    private Long id;
-//
-//    @Column(name = "name", nullable = false)
-//    private String name;
-//
-//    @Column(name = "email", nullable = false)
-//    private String email;
-//
-//    @Column(name = "password", nullable = false)
-//    private String password;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "gender")
-//    private Gender gender;
-//
-//    @Column(name = "phone")
-//    private String phone;
-//
-//    @Column(name = "profile_image")
-//    private String profileImage;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "provider")
-//    private Provider provider;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "status", nullable = false)
-//    private RegisterStatus registerStatus;
-//
-//    @PostPersist
-//    private void setRegisterStatus() {
-//        registerStatus = RegisterStatus.PENDING;
-//    }
-//}
+package umc.unimade.domain.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.unimade.global.registerStatus.RegisterStatus;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "seller_register")
+public class SellerRegister {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "request_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider")
+    private Provider provider;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private RegisterStatus registerStatus;
+
+    @PostPersist
+    private void setRegisterStatus() {
+        registerStatus = RegisterStatus.PENDING;
+    }
+}
