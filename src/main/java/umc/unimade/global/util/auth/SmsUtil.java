cbd2a3e7264fa3667dc2518f31f65fd8809a5f9f
park20011029 <rokmck156@gmail.com>
2024-07-25 00:15:58 +0900

    :sparkles: feat: 전화번호 인증 관련 로직

---
+++ src/main/java/umc/unimade/global/util/auth/SmsUtil.java
@@ -0,0 +1,67 @@
+package umc.unimade.global.util.auth;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Random;
+
+@Service
+public class SmsUtil {
+   // private final SmsCertification smsCertification;
+
+    @Value("${coolsms.apikey}")
+    private String apiKey;
+
+    @Value("${coolsms.apisecret}")
+    private String apiSecret;
+
+    @Value("${coolsms.fromnumber}")
+    private String fromNumber;
+
+    private String createRandomNumber() {
+        Random rand = new Random();
+        String randomNum = "";
+        for (int i = 0; i < 4; i++) {
+            String random = Integer.toString(rand.nextInt(10));
+            randomNum += random;
+        }
+
+        return randomNum;
+    }
+
+    private HashMap<String, String> makeParams(String to, String randomNum) {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("from", fromNumber);
+        params.put("type", "SMS");
+        params.put("app_version", "test app 1.2");
+        params.put("to", to);
+        params.put("text", "라인 친구해요 : " + randomNum);
+        return params;
+    }
+
+    // 인증번호 전송하기
+    public String sendSMS(String phonNumber) {
+        Message coolsms = new Message(apiKey, apiSecret);
+
+        // 랜덤한 인증 번호 생성
+        String randomNum = createRandomNumber();
+        System.out.println(randomNum);
+
+        // 발신 정보 설정
+        HashMap<String, String> params = makeParams(phonNumber, randomNum);
+
+        try {
+            JSONObject obj = (JSONObject) coolsms.send(params);
+            System.out.println(obj.toString());
+        } catch (CoolsmsException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+        }
+
+        return randomNum;
+    }
+}
