031edc70053c67d469d66ebb711179afcac9a433
jjin70 <jjin0434@naver.com>
2024-07-25 15:42:55 +0900

    :sparkles: feat: deadline 지난 상품 soldout으로 바꾸기 구현 (#57)

--- src/main/java/umc/unimade/domain/products/repository/ProductRepository.java
+++ src/main/java/umc/unimade/domain/products/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package umc.unimade.domain.products.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import umc.unimade.domain.products.entity.ProductStatus;
 import umc.unimade.domain.products.entity.Products;
@@ -11,4 +12,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Products, Long>, ProductsRepositoryCustom {
     List<Products> findTop4BySellerIdAndStatusOrderByCreatedAtDesc(Long sellerId, ProductStatus status);
 
+    @Query("SELECT p FROM Products p WHERE p.status = :status AND p.deadline < CURRENT_DATE")
+    List<Products> findExpiredProducts(ProductStatus status);
 }
