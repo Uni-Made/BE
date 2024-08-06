fa29571bc1b67b61f2299a062726940a56c83d77
moonyaeyoon <moonyaeyoon@naver.com>
2024-08-06 14:28:32 +0900

    style : 불필요한 라이브러리 제거

--- src/main/java/umc/unimade/global/config/FirebaseConfig.java
+++ src/main/java/umc/unimade/global/config/FirebaseConfig.java
@@ -7,8 +7,6 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
