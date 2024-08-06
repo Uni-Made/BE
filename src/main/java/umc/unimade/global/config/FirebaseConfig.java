f4a7a2fc01a106553680084f276af10b5f5c48f4
gomin0 <gomin0981113@naver.com>
2024-08-06 14:14:54 +0900

    :recycle: refactor: firebase key path yml로 변견 (#127)

--- src/main/java/umc/unimade/global/config/FirebaseConfig.java
+++ src/main/java/umc/unimade/global/config/FirebaseConfig.java
@@ -4,27 +4,33 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.keyPath}")
+    private String keyPath;
+    private static final Logger logger = Logger.getLogger(FirebaseConfig.class.getName());
+
     @PostConstruct
     public void init() {
-        try{
-            FileInputStream serviceAccount =
-                    new FileInputStream("BE/src/main/resources/serviceAccountKey.json");
-
+        try (InputStream serviceAccount = new ClassPathResource(keyPath).getInputStream()) {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
             FirebaseApp.initializeApp(options);
 
-        }catch(Exception e){
-            e.printStackTrace();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to initialize Firebase", e);
         }
     }
 }
