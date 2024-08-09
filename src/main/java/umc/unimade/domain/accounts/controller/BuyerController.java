199230d38046d4c8e699732b22a7a1c76c0e856c
park20011029 <rokmck156@gmail.com>
2024-08-09 23:11:02 +0900

    :sparkles: feat: 회원 정보 수정

--- src/main/java/umc/unimade/domain/accounts/controller/BuyerController.java
+++ src/main/java/umc/unimade/domain/accounts/controller/BuyerController.java
@@ -1,5 +1,6 @@
 package umc.unimade.domain.accounts.controller;
 
+import com.google.protobuf.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.*;
@@ -8,6 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import umc.unimade.domain.accounts.dto.BuyerInfoRequestDto;
+import umc.unimade.domain.accounts.dto.BuyerProfileRequestDto;
 import umc.unimade.domain.accounts.dto.SellerPageResponse;
 import umc.unimade.domain.orders.dto.BuyerOrderHistoryResponse;
 import umc.unimade.domain.accounts.dto.BuyerPageResponse;
@@ -128,5 +131,21 @@ public class BuyerController {
         Pageable pageable = PageRequest.of(page, size);
         SellerPageResponse sellerPage = buyerQueryService.getSellerPage(buyer, sellerId, sort, pageable);
         return ResponseEntity.ok(sellerPage);
+    }
+
+    @Tag(name = "Buyer", description = "구매자 정보수정")
+    @Operation(summary = "구매자 정보 수정")
+    @PatchMapping("/update/info")
+    public ApiResponse<Boolean> updateBuyerInfo(@LoginBuyer Buyer buyer,
+                               @RequestBody BuyerInfoRequestDto buyerInfoRequestDto) {
+        return ApiResponse.onSuccess(buyerCommandService.updateBuyerInfo(buyer, buyerInfoRequestDto));
+    }
+
+    @Tag(name = "Buyer", description = "구매자 프로필 수정")
+    @Operation(summary = "구매자 프로필 수정")
+    @PatchMapping("/update/profile")
+    public ApiResponse<Boolean> updateBuyerProfile(@LoginBuyer Buyer buyer,
+                                                   @RequestBody BuyerProfileRequestDto buyerProfileRequestDto) {
+        return ApiResponse.onSuccess(buyerCommandService.updateBuyerProfile(buyer, buyerProfileRequestDto));
     }
 }
